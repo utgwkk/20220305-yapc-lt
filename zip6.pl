e8734f37ee7a9ed0231cecac7e6836d31fc4d872
utgwkk <utagawakiki@gmail.com>
2022-03-05 13:48:10 +0900

    zip6

---
+++ zip6.pl
@@ -0,0 +1,6 @@
+use strict;
+use warnings;
+use List::MoreUtils qw(zip6);
+
+my @x = (1);
+zip6 @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x, @x;
